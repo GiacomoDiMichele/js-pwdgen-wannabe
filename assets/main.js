d3f5160a1dd31eea56d82173fdb77cd241503e24
Giacomo <giacomo.1936@gmail.com>
2020-10-12 16:22:12 +0200

    Initial commit

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+
+var nome_utente = prompt('Come ti chiami?');
+
+document.writeln(nome_utente);
+
+var cognome_utente = prompt('Qual è il tuo cognome');
+
+document.writeln(cognome_utente);
+
+var colore_preferito = prompt('Qual è il tuo colore preferito');
+
+document.writeln(colore_preferito);
