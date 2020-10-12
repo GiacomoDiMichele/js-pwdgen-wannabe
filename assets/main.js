00c5def16b5dd603eb9ba75e82eba99d9068648f
Giacomo <giacomo.1936@gmail.com>
2020-10-12 16:42:59 +0200

    impostato algoritmo per password

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,12 @@
 
+
 var nome_utente = prompt('Come ti chiami?');
 
-document.writeln(nome_utente);
 
 var cognome_utente = prompt('Qual è il tuo cognome');
 
-document.writeln(cognome_utente);
 
 var colore_preferito = prompt('Qual è il tuo colore preferito');
 
-document.writeln(colore_preferito);
+
+document.getElementById('numero_utente').innerHTML = nome_utente + cognome_utente + colore_preferito + '20';
